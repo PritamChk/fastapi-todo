abb405c2f34242b79affdbd1aef17ac28592a7aa
PRITAM CHAKRABORTY <pritam.chk98@gmail.com>
2022-02-01 15:07:56 +0530

    bacha geche

--- app/api/userAPI/schema/userReq.py
+++
@@ -1,8 +0,0 @@
-
-from pydantic import constr
-
-from app.api.userAPI.schema.userbase import User
-
-
-class UserRequest(User):
-    password: constr(min_length=6, max_length=32)
--- app/api/view.py
+++
@@ -1,7 +0,0 @@
-import imp
-from fastapi import APIRouter
-from app.api.userAPI.routes.user_router import USER_ROUTER
-
-router = APIRouter()
-
-router.include_router(USER_ROUTER)
